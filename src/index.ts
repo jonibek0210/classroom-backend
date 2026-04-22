26c31f6eb307750c9e2b28536e1fb58aed0de439
jonibek latipov <jonibeklatipov2022@gmail.com>
2026-04-22 07:19:40 +0500

    implement suggestions from coderabbit

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import subjectsRouter from "./routes/subjects";
 const app = express();
 const PORT = 8000;
 
+if (!process.env.FRONTEND_URL) throw new Error("FRONTEND_URL is not set in .env file");
+
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
